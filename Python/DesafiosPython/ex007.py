d5781b2ec1c8f8251a208ca8fafe4def7b4d9d27
José Ricardo Lemos <rlemos1997@gmail.com>
2024-10-20 23:53:18 -0300

    att

---
+++ Python/DesafiosPython/ex007.py
@@ -0,0 +1,7 @@
+print("CONTROLE DE TERRENOS")
+print("=" * 30)
+
+largura = float(input("Largura do terreno: "))
+comprimento = float(input("Digite o comprimento do terreno"))
+
+print(f"A área de um terreno {largura}x{comprimento} é de {largura*comprimento}m²")
